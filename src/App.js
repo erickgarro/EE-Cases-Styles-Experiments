e9d776f465d960ef5d06ec0367989d055d529977
erickgarro <erickgarro@gmail.com>
2022-05-01 22:01:37 +0200

    Implemented update of current task and completed task counters on reload

--- src/App.js
+++ src/App.js
@@ -28,6 +28,8 @@ let userTemplate = {
   gaveConsent: true
 };
 
+let sortedColors = ['word1', 'word2', 'word3'];
+
 /*
  * This is the main component of the application.
  * It is the entry point of the application.
@@ -36,11 +38,13 @@ function App() {
   const [cookie, setCookie] = useCookies();
   const [questions, setQuestions] = useState({});
   const [completedTasks, setCompletedTasks] = useState(0);
+  const [currentTask, setCurrentTask] = useState(1);
   const [hasAnswered, setHasAnswered] = useState(false);
   const [success, setSuccess] = useState(false);
   const [user, setUser] = useState(userTemplate);
   const [elapsedTime, setElapsedTime] = useState(0);
   const [responses, setResponses] = useState(responsesTemplate);
+  const [colors, setColors] = useState(() => sortedColors.sort(() => Math.random() - 0.5));
 
   // Fetch user ID and questions, either from the server or from the cookies and local storage
   useEffect(() => {
@@ -77,10 +81,16 @@ function App() {
       // Get questions from local storage
       setQuestions(() => JSON.parse(localStorage.getItem('questions')));
 
-      // Fetch answers in progress from local storage
+      // Get answers in progress from local storage
       let localResponses = localStorage.getItem('responses');
       if (localResponses) {
         setResponses(() => JSON.parse(localResponses));
+      }
+      //Get setCompletedTasks from local storage
+      let localCompletedTasks = localStorage.getItem('completedTasks');
+      if (localCompletedTasks) {
+        setCompletedTasks(() => JSON.parse(localCompletedTasks));
+        setCurrentTask(() => JSON.parse(localCompletedTasks));
       }
     }
   }, [setCookie, cookie.userId]);
@@ -95,15 +105,23 @@ function App() {
   function getOneOption(option, colorType, colors, caseStyle) {
     return (
       <div key={option.id} className='option'>
-        <button className={`option ${hasAnswered ? `${(option.correct) ? 'correct' : 'wrong'}` : ''}`} onClick={() => handleClick(option, completedTasks + 1)}>
+        <button className={`option ${hasAnswered ? `${(option.correct) ? 'correct' : 'wrong'}` : ''}`} onClick={() => handleWordSelection(option, currentTask)}>
           <span className={`${colorType === 'chromatic' ? colors[0] : ''}`}>{option.words[0]}</span>
-          {(caseStyle === 'kebab') ? <span className={caseStyle === 'kebab' ? 'hyphen' : ''}>-</span>: ''}
+          {(caseStyle === 'kebab') ? <span className={(colorType === 'chromatic' && caseStyle === 'kebab') ? 'hyphen' : ''}>-</span>: ''}
           <span className={`${colorType === 'chromatic' ? colors[1] : ''}`}>{option.words[1]}</span>
-          {(option.words.length > 2 && caseStyle === 'kebab') ? <span className={caseStyle === 'kebab' ? 'hyphen' : ''}>-</span>: ''}
+          {(option.words.length > 2 && caseStyle === 'kebab') ? <span className={(colorType === 'chromatic' && caseStyle === 'kebab') ? 'hyphen' : ''}>-</span>: ''}
           {option.words.length === 3 && <span className={`${colorType === 'chromatic' ? colors[2] : ''}`}>{option.words[2]}</span>}
         </button>
       </div>
     );
+  }
+
+  function getNexWord() {
+    setSuccess(() => false);
+    setHasAnswered(() => false);
+    setCompletedTasks(() => completedTasks + 1);
+    setCurrentTask(() => currentTask + 1);
+    setColors(() => sortedColors.sort(() => Math.random() - 0.5));
   }
 
   /*
@@ -112,35 +130,34 @@ function App() {
    * @return {String} - The html code for the options
    */
   function getOptions() {
-    let colors = ['word1', 'word2', 'word3'];
-    colors.sort(() => Math.random() - 0.5);
 
     return (
       <div className="App">
         <div className="container">
           <div className="control">
-            <p>Task {hasAnswered + 1} of {questions.questions.length}</p>
+            <p>Task {completedTasks + 1} of {questions.questions.length}</p>
           </div>
           <div className="words">
-            <h1>{questions.questions[completedTasks].words.join(' ')}</h1>
+            <h1>{questions.questions[currentTask].words.join(' ')}</h1>
+          </div>
+          <div>
+            <h2 className="statement">Can you spot the words?</h2>
           </div>
           <div className="answers">
             <div className='options-container'>
               {
                 // Map through the questions and get the options
-                questions.questions && questions.questions[completedTasks].options.map(option => {
-                  return getOneOption(option, questions.questions[completedTasks].color, colors, questions.questions[completedTasks].caseStyle);})
+                questions.questions && questions.questions[currentTask].options.map(option => {
+                  return getOneOption(option, questions.questions[currentTask].color, colors, questions.questions[currentTask].caseStyle);})
               }
             </div>
           </div>
          {success ? <div className={`feedback ${!hasAnswered ? 'hidden' : ''}`}>Well done!</div> :
            <div className={`feedback ${!hasAnswered ? 'hidden' : ''}`}>Oops! Not quite right.</div>}
           <div className="action">
-            {}
-            <button className={`continue ${!hasAnswered ? 'hidden' : ''}`}>Continue</button>
+            <button className={`continue ${!hasAnswered ? 'hidden' : ''}`} onClick={() => getNexWord()}>Continue</button>
           </div>
           <div className="footer">
-
           </div>
         </div>
       </div>
@@ -151,16 +168,21 @@ function App() {
    * This function adds an event listener is called when the user clicks on an option. It sets the success variable to true or false depending
    * on the property 'correct' of the option.
    */
-  function handleClick(option, taskId) {
-    setSuccess(() => option.correct);
-    setHasAnswered(() => true);
-    option.time = 0;
-    let selection = responses;
-   // add taskId as element of selection.response and assign option to it
-    selection.answers[taskId] = option;
-    setResponses(() => selection);
-    localStorage.setItem('responses', JSON.stringify(selection));
+  function handleWordSelection(option, taskId) {
+    if (!hasAnswered) {
+      setSuccess(() => option.correct);
+      setHasAnswered(() => true);
+      option.time = 0;
+      let selection = responses;
+      // add taskId as element of selection.response and assign option to it
+      selection.answers[taskId] = option;
+      setResponses(() => selection);
+      localStorage.setItem('responses', JSON.stringify(selection));
+      localStorage.setItem('completedTasks', JSON.stringify(completedTasks + 1));
+      //ToDo: add a timer to the task
+    }
   }
+
 
   return (
     // <Context.provider value={{ questions, dispatch }}>
