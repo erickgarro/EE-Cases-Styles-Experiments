5fc61ec399f995bf34f9d3117c02f8ca8832310e
erickgarro <erickgarro@gmail.com>
2022-05-01 21:00:32 +0200

    Enabled correct/wrong control, and handling responses locally

--- src/App.js
+++ src/App.js
@@ -10,11 +10,23 @@ import './App.css';
 import Context from './components/context';
 import {useState, useEffect} from "react";
 import { CookiesProvider, useCookies } from 'react-cookie';
-import createQuestions from './Questions';
 require('typeface-open-sans')
 
 const server = process.env.REACT_APP_SERVER;
 // const server = process.env.REACT_APP_LOCAL_SERVER;
+
+let responsesTemplate = {
+  user: {},
+  answers: {}
+};
+
+let userTemplate = {
+  id: '',
+  age : 0,
+  gender: '',
+  background: '',
+  gaveConsent: true
+};
 
 /*
  * This is the main component of the application.
@@ -23,66 +35,14 @@ const server = process.env.REACT_APP_SERVER;
 function App() {
   const [cookie, setCookie] = useCookies();
   const [questions, setQuestions] = useState({});
-  const [completedTasks, setTasks] = useState(0);
-  const [optionNumber, setOptionNumber] = useState(0);
+  const [completedTasks, setCompletedTasks] = useState(0);
   const [hasAnswered, setHasAnswered] = useState(false);
   const [success, setSuccess] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState({});
-  const [responses, dispatch] = useState(null); //ToDo: implement responses
-  let getOptionss = createQuestions();
+  const [user, setUser] = useState(userTemplate);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [responses, setResponses] = useState(responsesTemplate);
 
-  function getOneOption(option, colorType, colors, caseStyle) {
-    return (
-      <div key={option.id} className='option'>
-        <button id={option.id} className={`option ${hasAnswered ? `${(option.correct) ? 'correct' : 'wrong'}` : ''}`}>
-          <span className={`${colorType === 'chromatic' ? colors[0] : ''}`}>{option.words[0]}</span>
-          {(caseStyle === 'kebab') ? <span className={caseStyle === 'kebab' ? 'hyphen' : ''}>-</span>: ''}
-          <span className={`${colorType === 'chromatic' ? colors[1] : ''}`}>{option.words[1]}</span>
-          {(option.words.length > 2 && caseStyle === 'kebab') ? <span className={caseStyle === 'kebab' ? 'hyphen' : ''}>-</span>: ''}
-          {option.words.length === 3 && <span className={`${colorType === 'chromatic' ? colors[2] : ''}`}>{option.words[2]}</span>}
-        </button>
-      </div>
-    );
-  }
-
-  let colors = ['word1', 'word2', 'word3'];
-  colors.sort(() => Math.random() - 0.5);
-
-  function getOptions() {
-    return (
-      <div className="App">
-        <div className="container">
-          <div className="control">
-            <p>Task {hasAnswered + 1} of {questions.questions.length}</p>
-          </div>
-          <div className="words">
-            <h1>{questions.questions[completedTasks].words.join(' ')}</h1>
-          </div>
-          <div className="answers">
-            <div className='options-container'>
-              {
-                //map through the questions and get the options
-                questions.questions && questions.questions[completedTasks].options.map(option => {
-                      // getOneOption(option, colorType, colors, caseStyle)
-                  return getOneOption(option, questions.questions[completedTasks].color, colors, questions.questions[completedTasks].caseStyle);})
-              }
-            </div>
-          </div>
-         {success ? <div className={`feedback ${!hasAnswered ? 'hidden' : ''}`}>Well done!</div> :
-           <div className={`feedback ${!hasAnswered ? 'hidden' : ''}`}>Oops! Not quite right.</div>}
-          <div className="action">
-            {}
-            <button className={`continue ${!hasAnswered ? 'hidden' : ''}`}>Continue</button>
-          </div>
-          <div className="footer">
-
-          </div>
-        </div>
-      </div>
-    )
-  }
-
- // Fetch new user ID and questions
+  // Fetch user ID and questions, either from the server or from the cookies and local storage
   useEffect(() => {
     if (!cookie.userId) {
       try {
@@ -116,9 +76,91 @@ function App() {
     } else {
       // Get questions from local storage
       setQuestions(() => JSON.parse(localStorage.getItem('questions')));
+
+      // Fetch answers in progress from local storage
+      let localResponses = localStorage.getItem('responses');
+      if (localResponses) {
+        setResponses(() => JSON.parse(localResponses));
+      }
     }
   }, [setCookie, cookie.userId]);
 
+  /*
+   * This function generate the html code for one option of a question
+   *
+   * @param {Object} option - The option to generate the html code
+   * @param {Number} index - The index of the option
+   * @return {String} - The html code for the option
+   */
+  function getOneOption(option, colorType, colors, caseStyle) {
+    return (
+      <div key={option.id} className='option'>
+        <button className={`option ${hasAnswered ? `${(option.correct) ? 'correct' : 'wrong'}` : ''}`} onClick={() => handleClick(option, completedTasks + 1)}>
+          <span className={`${colorType === 'chromatic' ? colors[0] : ''}`}>{option.words[0]}</span>
+          {(caseStyle === 'kebab') ? <span className={caseStyle === 'kebab' ? 'hyphen' : ''}>-</span>: ''}
+          <span className={`${colorType === 'chromatic' ? colors[1] : ''}`}>{option.words[1]}</span>
+          {(option.words.length > 2 && caseStyle === 'kebab') ? <span className={caseStyle === 'kebab' ? 'hyphen' : ''}>-</span>: ''}
+          {option.words.length === 3 && <span className={`${colorType === 'chromatic' ? colors[2] : ''}`}>{option.words[2]}</span>}
+        </button>
+      </div>
+    );
+  }
+
+  /*
+   * This function generate the html code for all the options of a question
+   *
+   * @return {String} - The html code for the options
+   */
+  function getOptions() {
+    let colors = ['word1', 'word2', 'word3'];
+    colors.sort(() => Math.random() - 0.5);
+
+    return (
+      <div className="App">
+        <div className="container">
+          <div className="control">
+            <p>Task {hasAnswered + 1} of {questions.questions.length}</p>
+          </div>
+          <div className="words">
+            <h1>{questions.questions[completedTasks].words.join(' ')}</h1>
+          </div>
+          <div className="answers">
+            <div className='options-container'>
+              {
+                // Map through the questions and get the options
+                questions.questions && questions.questions[completedTasks].options.map(option => {
+                  return getOneOption(option, questions.questions[completedTasks].color, colors, questions.questions[completedTasks].caseStyle);})
+              }
+            </div>
+          </div>
+         {success ? <div className={`feedback ${!hasAnswered ? 'hidden' : ''}`}>Well done!</div> :
+           <div className={`feedback ${!hasAnswered ? 'hidden' : ''}`}>Oops! Not quite right.</div>}
+          <div className="action">
+            {}
+            <button className={`continue ${!hasAnswered ? 'hidden' : ''}`}>Continue</button>
+          </div>
+          <div className="footer">
+
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  /*
+   * This function adds an event listener is called when the user clicks on an option. It sets the success variable to true or false depending
+   * on the property 'correct' of the option.
+   */
+  function handleClick(option, taskId) {
+    setSuccess(() => option.correct);
+    setHasAnswered(() => true);
+    option.time = 0;
+    let selection = responses;
+   // add taskId as element of selection.response and assign option to it
+    selection.answers[taskId] = option;
+    setResponses(() => selection);
+    localStorage.setItem('responses', JSON.stringify(selection));
+  }
 
   return (
     // <Context.provider value={{ questions, dispatch }}>
